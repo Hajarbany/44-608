342f5db767679b4bca0d43f86bcaed8f0cdee153
Hajarbany <97689037+Hajarbany@users.noreply.github.com>
2022-01-21 20:01:39 -0600

    Add files via upload

---
+++ calc_max_HajarBanyalmarjeh.py
@@ -0,0 +1,16 @@
+Fig. 2.2: calc_max_HajarBanyalmarjeh.py
+"""Find the minimum of three values."""
+
+number1 = int(input('Enter first integer: '))
+number2 = int(input('Enter second integer: '))
+number3 = int(input('Enter third integer: '))
+
+minimum = number1  
+
+if number2 < minimum:
+    minimum = number2
+
+if number3 < minimum:
+    minimum = number3
+
+print('Minimum value is', minimum)
